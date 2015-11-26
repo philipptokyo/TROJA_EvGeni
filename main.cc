a613373beabb0822706bb2076ba97d75bf04e7ad
philipp <philipp@cns.s.u-tokyo.ac.jp>
2015-11-27 01:20:54 +0900

    commit before changing the generation of events from histogram to graph/spline

--- main.cc
+++ main.cc
@@ -49,7 +49,9 @@ Int_t main(Int_t argc, char **argv){
   
   //get information from textfile
   info->parse(argv[1]);
-  
+
+  TRandom3 *randomizer=new TRandom3();  
+  randomizer->SetSeed(0);
   
   TFile* infile = TFile::Open(info->fInfilenameFromReaction,"read");
   
@@ -159,15 +161,90 @@ Int_t main(Int_t argc, char **argv){
   }
   
   // histAll->Draw("colz");
-  
+
+  histAll->GetXaxis()->SetRangeUser(0.0, 180.0);  
   
   // save histogram to output rootfile
   TFile* outfile = new TFile(info->fRootfilename, "recreate");
   outfile->cd();
   histAll->Write();
 
+  
+
+
+  // generate events and write them to tree
+//   Double_t x=0.0, 
+//            y=0.0, 
+//            z=0.0; 
+  Double_t e=0.0, // energy in MeV 
+           t=0.0, // theta in rad
+           p=0.0; // phi in rad
+  Int_t eventNumber=0, pdgID=2212;
+
+  TTree *events = new TTree();
+  events->Branch("eventNumber", &eventNumber, "eventNumber/I");
+//   events->Branch("x",&x, "x/D");
+//   events->Branch("y",&y, "y/D");
+//   events->Branch("z",&z, "z/D");
+  events->Branch("energy",&e, "energy/D");
+  events->Branch("theta",&t, "theta/D");
+  events->Branch("phi",&p, "phi/D");
+  events->Branch("pdgID",&pdgID, "pdgID/I");
+
+
+  //printf("Got energy %f, theta %f\n", en, th);
+  
+  if(info->fNumberEvents<0){
+    printf("Error! Invalid number of events given: %i\nAboring...\n",info->fNumberEvents);
+    abort();
+  }else{
+    printf("Start generating events.\n");
+  }
+
+  for(Int_t i=0; i<info->fNumberEvents; i++){
+
+    eventNumber=i;
+
+    if(i%100000==0){
+      printf("%i events generated (%i requested)\n", i, info->fNumberEvents); 
+    }
+
+    t=181.0;
+    while(t>180.0){
+      histAll->GetRandom2(t,e);
+    }
+
+    t*=TMath::Pi()/180.0;
+    
+    p=randomizer->Uniform(2.0*TMath::Pi());
+    
+    TVector3 direction(0.0, 0.0, -1.0);
+    direction.SetMag(1.0);
+
+    direction.SetPhi(p);
+    direction.SetTheta(t);
+
+    //x=direction.X();
+    //y=direction.Y();
+    //z=direction.Z();
+
+    //printf("Got energy %lf, theta %lf, phi %lf,  x %lf, y %lf, z %lf\n", e, t, p, x, y, z);
+
+    pdgID=2212; //proton
+
+    events->Fill();
+  }
+
+
+
+  outfile->cd();
+
+  events->Write("events");
+
   outfile->Close();
   
+  printf("Events written to file '%s'\n", info->fRootfilename);
+  printf("Thank you and good by!\n");
   
   // if histograms shall be plotted, run theApp
   // otherwise program closes
